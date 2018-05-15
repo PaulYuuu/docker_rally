6770b82f1a7f3f5b79c2cd7c4dfa30c8933f7bd3
yuyihuang <yuyihuang@live.com>
2018-05-15 14:36:45 +0800

    Update 0.12.0

--- rally_automatic.sh
+++ rally_automatic.sh
@@ -3,52 +3,55 @@
 script_dir=$(cd `dirname $0`; pwd)
 cd $script_dir
 
-if [ ! -d testcase_result ];then
-    mkdir testcase_result
-fi
+time=`date +%H:%M:%S`
+mkdir -p testcase_result/$time
 
-testcase_file=testcase_result/rally_testcase.txt
-total_file=testcase_result/rally_total.txt
+testcase_file=testcase_result/$time/rally_testcase.txt
+total_file=testcase_result/$time/rally_total.txt
 rally_task_dir=source/samples/tasks/scenarios
 
 #keystone
 keystone_case=`find $rally_task_dir/keystone -name "*.yaml"`
-keystone_num=`grep -rn '\<KeystoneBasic' $keystone_case | wc -l`
+keystone_num=`grep -rn '\<Keystone' $keystone_case | wc -l`
 echo "Keystone Testcases Number: "$keystone_num > $total_file
-echo "#Keystone" > $testcase_file
-keystone_name=`grep -rn '\<KeystoneBasic' $keystone_case | awk '{print NR":",$2}' >> $testcase_file`
+echo "Keystone" > $testcase_file
+grep -rn '\<Keystone' $keystone_case | awk '{print NR":",$2}' >> $testcase_file
 sed -i 's/Keystone.*\.//g' $testcase_file
 
 #glance
 glance_case=`find $rally_task_dir/glance -name "*.yaml"`
-glance_num=`grep -rn '\<GlanceImages' $glance_case | wc -l`
+glance_num=`grep -rn '\<Glance' $glance_case | wc -l`
 echo "Glance Testcases Number: "$glance_num >> $total_file
-echo "#Glance" >> $testcase_file
-glance_name=`grep -rn '\<GlanceImages' $glance_case | awk '{print NR":",$2}' >> $testcase_file`
+echo "" >> $testcase_file
+echo "Glance" >> $testcase_file
+grep -rn '\<Glance' $glance_case | awk '{print NR":",$2}' >> $testcase_file
 sed -i 's/Glance.*\.//g' $testcase_file
 
 #nova
 nova_case=`find $rally_task_dir/nova -name "*.yaml"`
 nova_num=`grep -rn '\<Nova' $nova_case | wc -l`
 echo "Nova Testcases Number: "$nova_num >> $total_file
-echo "#Nova" >> $testcase_file
-nova_name=`grep -rn '\<Nova' $nova_case | awk '{print NR":",$2}' >> $testcase_file`
+echo "" >> $testcase_file
+echo "Nova" >> $testcase_file
+grep -rn '\<Nova' $nova_case | awk '{print NR":",$2}' >> $testcase_file
 sed -i 's/Nova.*\.//g' $testcase_file
 
 #neutron
 neutron_case=`find $rally_task_dir/neutron -name "*.yaml"`
 neutron_num=`grep -rn '\<Neutron' $neutron_case | wc -l`
 echo "Neutron Testcases Number: "$neutron_num >> $total_file
-echo "#Neutron" >> $testcase_file
-neutron_name=`grep -rn '\<Neutron' $neutron_case | awk '{print NR":",$2}' >> $testcase_file`
+echo "" >> $testcase_file
+echo "Neutron" >> $testcase_file
+grep -rn '\<Neutron' $neutron_case | awk '{print NR":",$2}' >> $testcase_file
 sed -i 's/Neutron.*\.//g' $testcase_file
 
 #cinder
 cinder_case=`find $rally_task_dir/cinder -name "*.yaml"`
-cinder_num=`grep -rn '\<CinderVolumes' $cinder_case | wc -l`
+cinder_num=`grep -rn '\<Cinder' $cinder_case | wc -l`
 echo "Cinder Testcases Number: "$cinder_num >> $total_file
-echo "#Cinder" >> $testcase_file
-cinder_name=`grep -rn '\<CinderVolumes' $cinder_case | awk '{print NR":",$2}' >> $testcase_file`
+echo "" >> $testcase_file
+echo "Cinder" >> $testcase_file
+grep -rn '\<Cinder' $cinder_case | awk '{print NR":",$2}' >> $testcase_file
 sed -i 's/Cinder.*\.//g' $testcase_file
 
 #total
@@ -57,7 +60,8 @@ echo "Total Testcases Number: $total" >> $total_file
 sed -i 's/:$//' $testcase_file
 
 # Run Scripts tests
-for i in rally_scripts/*.sh
+cd testcase_result/$time
+for i in ../../rally_scripts/*.sh
 do
     bash $i
 done
--- rally_scripts/cinder.sh
+++ rally_scripts/cinder.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 
-script_dir=$(cd `dirname $0`; pwd)
-cd $script_dir
+mkdir -p cinder/{log,html}
 
-if [ ! -d ../testcase_result/cinder ];then
-    mkdir ../testcase_result/cinder
-fi
-
-rally_task_dir=source/samples/tasks/scenarios
-cinder_case=`find ../$rally_task_dir/cinder -name "*.yaml"`
+rally_task_dir=../../source/samples/tasks/scenarios
+cinder_case=`find $rally_task_dir/cinder -name "*.yaml"`
 
 for case in $cinder_case
 do
+	#Create rally log
     name=`echo $case | awk -F "/" '{print $NF}' | cut -d "." -f 1`
-    rally --log-file ../testcase_result/cinder/$name.log task start --task $case
+    rally --log-file cinder/log/$name.log task start --task $case
     #Create rally report
     uuid=`rally task status | awk '{print $2}' | tr -d :`
-    rally task report $uuid --out ../testcase_result/cinder"_"$name"_"$uuid.html
+    rally task report $uuid --out cinder/html/$name.html
 done
